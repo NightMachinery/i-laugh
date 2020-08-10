b4a0fcabc4ce4bb46f870df436229d849971a120
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-11 03:29:33 +0430

    Updated deps

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject i-laugh "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :jvm-opts ^:replace [] 
+  ;; :jvm-opts ^:replace []
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [overtone "0.10.3"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [overtone "0.10.6"]]
+  :native-path "native"
+  :source-paths ["src"]
+  )
